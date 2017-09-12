861d157d0cbec08698d618d2db0a365e3a808961
wp1016 <wp290189594@163.com>
2017-09-12 21:07:56 +0800

    vux-demo Signed-off-by: wp1016 <wp290189594@163.com>

--- src/pages/event_detail/event_detail.vue
+++ src/pages/event_detail/event_detail.vue
@@ -1,25 +1,41 @@
 <template>
   <div>
-    <view-box ref="viewBox" :body-padding-top="'40px'">
-      <x-header slot="header" :title="title" :left-options="{backText: ''}"></x-header>
+    <x-header slot="header" :title="title" :left-options="{backText: ''}"></x-header>
+
+    <view-box ref="viewBox" :body-padding-top="'40px'" :body-padding-bottom="'0'">
       <div slot="default">
-        <x-button link="/home">to home</x-button>
+        <tab>
+          <tab-item selected>交流讨论</tab-item>
+          <tab-item>活动文章</tab-item>
+          <tab-item>活动资源</tab-item>
+          <tab-item>活动相册</tab-item>
+        </tab>
+        <transition>
+        </transition>
       </div>
     </view-box>
   </div>
 </template>
 <script>
-  import {ViewBox,XHeader,XButton} from 'vux'
+  import {ViewBox,XHeader,XButton,Tab,TabItem} from 'vux'
   export default {
     data(){
       return{
-        title:'活动详情页'
+        title:''
       }
+    },
+    mounted(){
+      this.title=this.$route.params.title
     },
     components:{
       ViewBox,
       XHeader,
-      XButton
+      XButton,
+      Tab,
+      TabItem
     }
   }
 </script>
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,33 @@
 import App from '../App'
 
 const home = r => require.ensure([], () => r(require('../pages/home/home')), 'home');
-const helloFromVux=r=> require.ensure([],()=>r(require('../components/HelloFromVux')),'helloFromVux')
+const helloFromVux = r => require.ensure([], () => r(require('../components/HelloFromVux')), 'helloFromVux')
 const event_list = r => require.ensure([], () => r(require('../pages/event_list/event_list')), 'event_list');
 const event_detail = r => require.ensure([], () => r(require('../pages/event_detail/event_detail')), 'event_detail');
 
 
 
-export default [
-    {
-      path: '/',
-      component: App,
-      children:[
-        {
-          path:'',
-          redirect:'/home'
+export default [{
+    path: '/',
+    component: App,
+    children: [{
+            path: '',
+            redirect: '/home'
         },
         {
-          path:'/home',
-          component:home,
+            path: '/home',
+            name: 'home',
+            component: home,
         },
         {
-          path:'/event_list',
-          component:event_list
+            path: '/event_list',
+            home: 'event_list',
+            component: event_list
         },
         {
-          path:'/event_detail',
-          component:event_detail
+            path: '/event_detail/:title',
+            name: 'event_detail',
+            component: event_detail,
         }
-      ]
-    }
-  ]
+    ]
+}]
